63234cfc5be7380ff5d26bf0925f4babb5fbdb31
Pietro Braione <pietro.braione@gmail.com>
2017-06-20 14:10:31 +0200

    Added settings for Pietro's machine

--- src/common/Settings.java
+++ src/common/Settings.java
@@ -4,13 +4,24 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class Settings {
-	public static final Path WORKSPACE      = Paths.get("E:/EclipseNeon/Workspace");
-	public static final Path SRC_PATH       = Paths.get(WORKSPACE.toString(), "DynamicSym", "src");
-	public static final Path BIN_PATH       = Paths.get(WORKSPACE.toString(), "DynamicSym", "bin");
+	//Massimo's settings
+	/*
 	public static final Path JBSE_PATH      = Paths.get("E:/jbse/jbse-master", "bin");
-	public static final Path TMP_BASE_PATH  = Paths.get(WORKSPACE.toString(), "DynamicSym", "tmp");
 	public static final Path EVOSUITE_PATH  = Paths.get("C:/Users", "Maxximo", "git", "sushi", "lib", "evosuite-shaded-1.0.3.jar");
 	public static final Path SUSHI_LIB_PATH = Paths.get("C:/Users", "Maxximo", "git", "sushi-lib", "bin");
 	public static final Path Z3_PATH        = Paths.get("E:/", "z3-master", "build", "z3");
-	public static final Path OUT_PATH       = Paths.get(WORKSPACE.toString(), "DynamicSym", "testCase");
+	public static final Path WORKSPACE      = Paths.get("E:/EclipseNeon/Workspace/DynamicSym");
+	*/
+	//Pietro's settings
+	public static final Path JBSE_PATH      = Paths.get("/Users", "pietro", "git", "sushi", "bin");
+	public static final Path EVOSUITE_PATH  = Paths.get("/Users", "pietro", "git", "sushi", "lib", "evosuite-shaded-1.0.3.jar");
+	public static final Path SUSHI_LIB_PATH = Paths.get("/Users", "pietro", "git", "sushi-lib", "bin");
+	public static final Path Z3_PATH        = Paths.get("/opt", "local", "bin", "z3");
+	public static final Path WORKSPACE      = Paths.get("/Users/pietro/git/jbse-concolic/");
+	
+	//these are good for everyone
+	public static final Path SRC_PATH       = WORKSPACE.resolve("src");
+	public static final Path BIN_PATH       = WORKSPACE.resolve("bin");
+	public static final Path OUT_PATH       = WORKSPACE.resolve("testCase");
+	public static final Path TMP_BASE_PATH  = WORKSPACE.resolve("tmp");
 }
