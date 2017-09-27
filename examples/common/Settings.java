16af97e59b6abe77b9b226789d48b6608c9c3138
Pietro Braione <pietro.braione@gmail.com>
2017-09-27 10:20:44 +0200

    Fix for new Maven structure of sushi project

--- examples/common/Settings.java
+++ examples/common/Settings.java
@@ -16,8 +16,8 @@ public final class Settings {
 	
 	//Pietro's settings
 	public static final Path JBSE_PATH      = Paths.get("/Users", "pietro", "git", "jbse", "target", "classes");
-	public static final Path EVOSUITE_PATH  = Paths.get("/Users", "pietro", "git", "sushi", "lib", "evosuite-shaded-1.0.3.jar");
-	public static final Path SUSHI_LIB_PATH = Paths.get("/Users", "pietro", "git", "sushi-lib", "bin");
+	public static final Path EVOSUITE_PATH  = Paths.get("/Users", "pietro", "git", "sushi", "evosuite", "evosuite-shaded-1.0.3.jar");
+	public static final Path SUSHI_LIB_PATH = Paths.get("/Users", "pietro", "git", "sushi", "runtime" , "target", "classes");
 	public static final Path Z3_PATH        = Paths.get("/opt", "local", "bin", "z3");
 	public static final Path WORKSPACE      = Paths.get("/Users/pietro/git/jbse-concolic/");
 	
@@ -27,5 +27,5 @@ public final class Settings {
 	public static final Path BIN_PATH       = WORKSPACE.resolve("bin");
 	public static final Path OUT_PATH       = WORKSPACE.resolve("testCase");
 	public static final Path TMP_BASE_PATH  = WORKSPACE.resolve("tmp");
-	public static final Path JRE_PATH       = JBSE_PATH.resolve("../../data/jre/rt.jar");
+	public static final Path JRE_PATH       = JBSE_PATH.resolve("../../data/jre1.8.0/rt.jar");
 }
