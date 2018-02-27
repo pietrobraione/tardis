b95ff1d4828273c8142861cde09be2cde63420c6
Pietro Braione <pietro.braione@gmail.com>
2018-02-27 12:03:24 +0100

    Add array example

---
+++ examples/array/RunArray.java
@@ -0,0 +1,43 @@
+package array;
+
+import java.util.concurrent.TimeUnit;
+
+import common.Settings;
+import exec.Main;
+import exec.Options;
+
+public class RunArray {
+	public static void main(String[] s) {
+		final String className = "array/Array";
+		final String parametersSignature = "([IIII)Z";
+		final String methodName  = "getBool";
+		final String testClass = "array/Test";
+		final String testMethodSignature = "()V";
+		final String testMethod = "test0";
+		final int maxDepth = 50;
+		final int numOfThreads = 5;
+		final long timeBudgetDuration = 10;
+		final TimeUnit timeBudgetTimeUnit = TimeUnit.MINUTES;
+		
+		final Options o = new Options();
+		o.setTargetMethod(className, parametersSignature, methodName);
+		o.setInitialTestCase(testClass, testMethodSignature, testMethod);
+		o.setMaxDepth(maxDepth);
+		o.setNumOfThreads(numOfThreads);
+		o.setTmpDirectoryBase(Settings.TMP_BASE_PATH);
+		o.setZ3Path(Settings.Z3_PATH);
+		o.setJBSELibraryPath(Settings.JBSE_PATH);
+		o.setJREPath(Settings.JRE_PATH);
+		o.setBinPath(Settings.BIN_PATH);
+		o.setOutDirectory(Settings.OUT_PATH);
+		o.setSushiLibPath(Settings.SUSHI_LIB_PATH);
+		o.setEvosuitePath(Settings.EVOSUITE_MOSA_PATH);
+		o.setUseMOSA(true);
+		o.setNumMOSATargets(5);
+		o.setGlobalTimeBudgetDuration(timeBudgetDuration);
+		o.setGlobalTimeBudgetUnit(timeBudgetTimeUnit);
+	
+		final Main m = new Main(o);
+		m.start();
+	}
+}
---
+++ examples/array/Test.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Test {
+	public void test0(){
+		Array t = new Array();
+		int a = 1;
+		int b = 1;
+		int c = 1;
+
+		int[] array = new int[] {1,2,3,4,5,65,6};
+
+		t.getBool(array, a, b, c);
+	}
+}
