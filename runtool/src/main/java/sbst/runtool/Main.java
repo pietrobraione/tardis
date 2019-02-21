8db885866712136f7c88514393bbaea1c636a417
Pietro Braione <pietro.braione@gmail.com>
2019-02-21 21:30:38 +0100

    Make runtool path-independent

--- runtool/src/main/java/sbst/runtool/Main.java
+++ runtool/src/main/java/sbst/runtool/Main.java
@@ -5,9 +5,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] s) throws IOException {
-		final TardisTool tool = new TardisTool();
-		final RunTool runtool = new RunTool(tool, new InputStreamReader(System.in), new OutputStreamWriter(System.out));
-		runtool.run();	
-	}
+    public static void main(String[] s) throws IOException {
+        final TardisTool tool = new TardisTool(s[0]);
+        final RunTool runtool = new RunTool(tool, new InputStreamReader(System.in), new OutputStreamWriter(System.out));
+        runtool.run();	
+    }
 }
--- runtool/src/main/java/sbst/runtool/TardisTool.java
+++ runtool/src/main/java/sbst/runtool/TardisTool.java
@@ -14,91 +14,99 @@ import java.util.Date;
 import java.util.List;
 
 public class TardisTool implements ITestingTool {
-	private static final String HOME           = "/home/tardis";
-	private static final String TARDIS         = HOME + "/lib/tardis-master-0.1.0-SNAPSHOT.jar";
-	private static final String SUSHI_LIB      = HOME + "/lib/sushi-lib-0.2.0-SNAPSHOT.jar";
-	private static final String JBSE_LIB       = HOME + "/lib/jbse-0.9.0-SNAPSHOT-shaded.jar";
-	private static final String EVOSUITE_LIB   = HOME + "/lib/evosuite-shaded-1.0.6-SNAPSHOT.jar";
-	private static final String ARGS4J_LIB     = HOME + "/lib/args4j-2.32.jar";
-	private static final String TOOLS_LIB      = "/usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar";
-	private static final String Z3_BIN         = HOME + "/opt/bin/z3";
-	private static final String TMP_DIR        = HOME + "/temp/data";
-	private static final String OUT_DIR        = HOME + "/temp/testcases";
-	
-	private final String classPathTardis;
-	private String classPathSUT;
+    private final String TARDIS;
+    private final String Z3_BIN;
+    private final String TMP_DIR;
+    private final String OUT_DIR;
+    private final String SUSHI_LIB;
+    private final String JBSE_LIB;
+    private final String EVOSUITE_LIB;
+    private final String ARGS4J_LIB;
+    private final String TOOLS_LIB;
 
-	public TardisTool() {
-		final StringBuilder sb = new StringBuilder();
-		sb.append(TARDIS); sb.append(":");
-		sb.append(SUSHI_LIB); sb.append(":");
-		sb.append(JBSE_LIB); sb.append(":");
-		sb.append(EVOSUITE_LIB); sb.append(":");
-		sb.append(ARGS4J_LIB); sb.append(":");
-		sb.append(TOOLS_LIB);
-		this.classPathTardis = sb.toString();
-	}
+    private final String classPathTardis;
+    private String classPathSUT;
 
-	@Override
-	public List<File> getExtraClassPath() {
-		return Collections.emptyList();
-	}
-	
-	@Override
-	public void initialize(File src, File bin, List<File> classPath) {
-		final StringBuilder sb = new StringBuilder();
-		sb.append(bin);
-		for (File f : classPath) {
-			sb.append(":");
-			sb.append(f);
-		}
-		this.classPathSUT = sb.toString();
-	}
+    public TardisTool(String currentDirectory) {
+        TARDIS         = currentDirectory + "/lib/tardis-master-0.1.0-SNAPSHOT.jar";
+        Z3_BIN         = currentDirectory + "/opt/bin/z3";
+        TMP_DIR        = currentDirectory + "/temp/data";
+        OUT_DIR        = currentDirectory + "/temp/testcases";
+        SUSHI_LIB      = currentDirectory + "/lib/sushi-lib-0.2.0-SNAPSHOT.jar";
+        JBSE_LIB       = currentDirectory + "/lib/jbse-0.9.0-SNAPSHOT-shaded.jar";
+        EVOSUITE_LIB   = currentDirectory + "/lib/evosuite-shaded-1.0.6-SNAPSHOT.jar";
+        ARGS4J_LIB     = currentDirectory + "/lib/args4j-2.32.jar";
+        TOOLS_LIB      = System.getProperty("java.home") + "/lib/tools.jar";
+        final StringBuilder sb = new StringBuilder();
+        sb.append(TARDIS); sb.append(":");
+        sb.append(SUSHI_LIB); sb.append(":");
+        sb.append(JBSE_LIB); sb.append(":");
+        sb.append(EVOSUITE_LIB); sb.append(":");
+        sb.append(ARGS4J_LIB); sb.append(":");
+        sb.append(TOOLS_LIB);
+        this.classPathTardis = sb.toString();
+    }
 
-	@Override
-	public void run(String cName, long timeBudget) {
-		try {
-			final ProcessBuilder pbuilder = new ProcessBuilder(
-					"java", "-Xms16G", "-Xmx16G", 
-					"-classpath", this.classPathTardis,
-					"tardis.Main", 
-					"-sushi_lib", SUSHI_LIB, "-jbse_lib", JBSE_LIB, "-evosuite", EVOSUITE_LIB, "-z3", Z3_BIN,
-					"-use_mosa", "-num_mosa_targets", "5", 
-					"-num_threads", "5", "-max_depth", "50",
-					"-tmp_base", TMP_DIR, "-out", OUT_DIR, 
-					"-global_time_budget_duration", Long.toString(timeBudget), "-global_time_budget_unit", "SECONDS",
-					"-classes", this.classPathSUT, "-target_class", cName.replace('.', '/')
-			);
+    @Override
+    public List<File> getExtraClassPath() {
+        return Collections.emptyList();
+    }
 
-			pbuilder.redirectErrorStream(true);
+    @Override
+    public void initialize(File src, File bin, List<File> classPath) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(bin);
+        for (File f : classPath) {
+            sb.append(":");
+            sb.append(f);
+        }
+        this.classPathSUT = sb.toString();
+    }
 
-			final Process process;
-			final InputStreamReader stdout;
-			final InputStream stderr;
-			final OutputStreamWriter stdin;
-			process = pbuilder.start();
-			stderr = process.getErrorStream();
-			stdout = new InputStreamReader(process.getInputStream());
-			stdin = new OutputStreamWriter(process.getOutputStream());
+    @Override
+    public void run(String cName, long timeBudget) {
+        try {
+            final ProcessBuilder pbuilder = new ProcessBuilder(
+               "java", "-Xms16G", "-Xmx16G", 
+               "-classpath", this.classPathTardis,
+               "tardis.Main", 
+               "-sushi_lib", SUSHI_LIB, "-jbse_lib", JBSE_LIB, "-evosuite", EVOSUITE_LIB, "-z3", Z3_BIN,
+               "-use_mosa", "-num_mosa_targets", "5", 
+               "-num_threads", "5", "-max_depth", "50",
+               "-tmp_base", TMP_DIR, "-out", OUT_DIR, 
+               "-global_time_budget_duration", Long.toString(timeBudget), "-global_time_budget_unit", "SECONDS",
+               "-classes", this.classPathSUT, "-target_class", cName.replace('.', '/')
+            );
 
-			final PrintStream dump = new PrintStream(new FileOutputStream(new File(TMP_DIR + "/" + new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Date()) + ".txt"), false));
-			dump.println(pbuilder.command().toString());
-			final BufferedReader reader = new BufferedReader(stdout);
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				dump.println(line);
-			}
-			dump.close();
-			reader.close();
+            pbuilder.redirectErrorStream(true);
 
-			process.waitFor();
+            final Process process;
+            final InputStreamReader stdout;
+            final InputStream stderr;
+            final OutputStreamWriter stdin;
+            process = pbuilder.start();
+            stderr = process.getErrorStream();
+            stdout = new InputStreamReader(process.getInputStream());
+            stdin = new OutputStreamWriter(process.getOutputStream());
 
-			if (stdout != null) stdout.close();
-			if (stdin != null) stdin.close();
-			if (stderr != null) stderr.close();
-			if (process !=null) process.destroy();
-		} catch (IOException | InterruptedException e) {
+            final PrintStream dump = new PrintStream(new FileOutputStream(new File(TMP_DIR + "/" + new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Date()) + ".txt"), false));
+            dump.println(pbuilder.command().toString());
+            final BufferedReader reader = new BufferedReader(stdout);
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                dump.println(line);
+            }
+            dump.close();
+            reader.close();
 
-		}
-	}
+            process.waitFor();
+
+            if (stdout != null) stdout.close();
+            if (stdin != null) stdin.close();
+            if (stderr != null) stderr.close();
+            if (process !=null) process.destroy();
+        } catch (IOException | InterruptedException e) {
+            //does nothing
+        }
+    }
 }
