9f4c8effdb0a5ffcd5c6249d3cb144def0d0602f
Pietro Braione <pietro.braione@gmail.com>
2021-03-22 18:10:17 +0100

    Refactor

--- master/src/main/java/tardis/implementation/SlicingManager.java
+++ master/src/main/java/tardis/implementation/SlicingManager.java
@@ -12,15 +12,19 @@ import java.util.regex.Pattern;
 import jbse.mem.Clause;
 import jbse.mem.ClauseAssume;
 import jbse.mem.ClauseAssumeReferenceSymbolic;
+import jbse.val.Any;
 import jbse.val.Expression;
 import jbse.val.NarrowingConversion;
 import jbse.val.Primitive;
+import jbse.val.PrimitiveSymbolicApply;
 import jbse.val.PrimitiveSymbolicMember;
 import jbse.val.PrimitiveSymbolicMemberArray;
 import jbse.val.PrimitiveSymbolicMemberArrayLength;
+import jbse.val.ReferenceSymbolic;
 import jbse.val.Simplex;
 import jbse.val.Symbolic;
-import jbse.val.SymbolicMember;
+import jbse.val.Term;
+import jbse.val.Value;
 import jbse.val.WideningConversion;
 
 /**
@@ -55,9 +59,8 @@ public class SlicingManager {
             final HashSet<String> set = new HashSet<>();
             if (c instanceof ClauseAssume) {
                 final Primitive p = ((ClauseAssume) c).getCondition();
-                if (p instanceof Expression) {
-                    originSets.add(getContainer(p, set));
-                }
+                getContainerPrimitive(p, set);
+                originSets.add(set);
             } else if (c instanceof ClauseAssumeReferenceSymbolic) {
                 final String ref = ((ClauseAssumeReferenceSymbolic) c).getReference().asOriginString();
                 set.add(splitByDot(ref));
@@ -124,155 +127,56 @@ public class SlicingManager {
      * Finds containers of all elements of the ClauseAssume clause and add them to the set;
      * Workflow: if operand is an expression, go deeper
      */
-    //TODO probably doesn't handle all cases
-    public static HashSet<String> getContainer(Primitive p, HashSet<String> set) {
-        if (((Expression) p).isUnary()) {
-            Primitive op = ((Expression) p).getOperand();
-            if (op instanceof Simplex) {
-                //do nothing
-            }
+    
+    public static void getContainerPrimitive(Primitive p, HashSet<String> set) {
+        if (p instanceof Simplex || p instanceof Any || p instanceof Term) {
+            //do nothing, they have no container
+        } else if (p instanceof PrimitiveSymbolicMemberArrayLength || p instanceof PrimitiveSymbolicMemberArray) {
             //special case for arrays
-            else if (op instanceof PrimitiveSymbolicMemberArrayLength || op instanceof PrimitiveSymbolicMemberArray) {
-                set.add(op.toString());
-            }
-            else if (op instanceof WideningConversion) {
-                getContainerWideningConversion(op, set);
-            }
-            else if (op instanceof NarrowingConversion) {
-                getContainerNarrowingConversion (op, set);
-            }
-            else if (op instanceof Expression) {
-                getContainer (op, set);
-            }
-            else {
-                try {
-                    set.add(((SymbolicMember) op).getContainer().asOriginString());
-                }
-                catch (Exception e) {
-                    //give up
-                    set.add(splitByDot(((Symbolic) op).asOriginString()));
-                }
-            }
+            set.add(p.toString());
+        } else if (p instanceof PrimitiveSymbolicMember) { //PrimitiveSymbolicMemberField
+            set.add(((PrimitiveSymbolicMember) p).getContainer().asOriginString());
+        } else if (p instanceof Expression) {
+            getContainerExpression((Expression) p, set);
+        } else if (p instanceof PrimitiveSymbolicApply) {
+            getContainerPrimitiveSymbolicApply((PrimitiveSymbolicApply) p, set);
+        } else if (p instanceof WideningConversion) {
+            getContainerPrimitive(((WideningConversion) p).getArg(), set);
+        } else if (p instanceof NarrowingConversion) {
+            getContainerPrimitive(((NarrowingConversion) p).getArg(), set);
+        } else { //PrimitiveSymbolicHashCode, PrimitiveSymbolicLocalVariable
+            set.add(splitByDot(((Symbolic) p).asOriginString()));
         }
-        else {
-            Primitive firstOp = ((Expression) p).getFirstOperand();
-            Primitive secondOp = ((Expression) p).getSecondOperand();
-            if (firstOp instanceof Simplex) {
-                //do nothing
-            }
-            //special case for arrays
-            else if (firstOp instanceof PrimitiveSymbolicMemberArrayLength || firstOp instanceof PrimitiveSymbolicMemberArray) {
-                set.add(firstOp.toString());
-            }
-            else if (firstOp instanceof WideningConversion) {
-                getContainerWideningConversion(firstOp, set);
-            }
-            else if (firstOp instanceof NarrowingConversion) {
-                getContainerNarrowingConversion (firstOp, set);
-            }
-            else if (firstOp instanceof Expression) {
-                getContainer (firstOp, set);
-            }
-            else {
-                try {
-                    set.add(((SymbolicMember) firstOp).getContainer().asOriginString());
-                }
-                catch (Exception e) {
-                    //give up
-                    set.add(splitByDot(((Symbolic) firstOp).asOriginString()));
-                }
-            }
-
-            if (secondOp instanceof Simplex) {
-                //do nothing
-            }
-            //special case for arrays
-            else if (secondOp instanceof PrimitiveSymbolicMemberArrayLength || secondOp instanceof PrimitiveSymbolicMemberArray) {
-                set.add(secondOp.toString());
-            }
-            else if (secondOp instanceof WideningConversion) {
-                getContainerWideningConversion(secondOp, set);
-            }
-            else if (secondOp instanceof NarrowingConversion) {
-                getContainerNarrowingConversion (secondOp, set);
-            }
-            else if (secondOp instanceof Expression) {
-                getContainer (secondOp, set);
-            }
-            else {
-                try {
-                    set.add(((SymbolicMember) secondOp).getContainer().asOriginString());
-                }
-                catch (Exception e) {
-                    //give up
-                    set.add(splitByDot(((Symbolic) secondOp).asOriginString()));
-                }
-            }
+    }
+    
+    public static void getContainerExpression(Expression p, HashSet<String> set) {
+        if (p.isUnary()) {
+            final Primitive operand = p.getOperand();
+            getContainerPrimitive(operand, set);
+        } else {
+            final Primitive firstOperand = p.getFirstOperand();
+            getContainerPrimitive(firstOperand, set);
+            final Primitive secondOperand = p.getSecondOperand();
+            getContainerPrimitive(secondOperand, set);
         }
-        return set;
     }
 
-    //find containers of all elements of the ClauseAssume clause in the case of the operator is instance of WideningConversion
-    public static HashSet<String> getContainerWideningConversion (Primitive operator, HashSet<String> set) {
-        //special case for arrays
-        if (((WideningConversion) operator).getArg() instanceof PrimitiveSymbolicMemberArrayLength || ((WideningConversion) operator).getArg() instanceof PrimitiveSymbolicMemberArray) {
-            set.add(operator.toString());
-        }
-        else if (((WideningConversion) operator).getArg() instanceof WideningConversion) {
-            getContainerWideningConversion(((WideningConversion) operator).getArg(), set);
-        }
-        else if (((WideningConversion) operator).getArg() instanceof NarrowingConversion) {
-            getContainerNarrowingConversion(((WideningConversion) operator).getArg(), set);
-        }
-        else if (((WideningConversion) operator).getArg() instanceof Expression) {
-            getContainer (((WideningConversion) operator).getArg(), set);
-        }
-        else {
-            try {
-                set.add(((PrimitiveSymbolicMember) ((WideningConversion) operator).getArg()).getContainer().asOriginString());
-            }
-            catch (Exception e) {
-                //give up
-                set.add(splitByDot(((Symbolic) operator).asOriginString()));
+    public static void getContainerPrimitiveSymbolicApply(PrimitiveSymbolicApply p, HashSet<String> set) {
+        for (Value arg : p.getArgs()) {
+            if (arg instanceof Primitive) {
+                getContainerPrimitive((Primitive) arg, set);
+            } else if (arg instanceof ReferenceSymbolic) {
+                set.add(splitByDot(((ReferenceSymbolic) arg).asOriginString()));
             }
         }
-        return set;
     }
 
-    //find containers of all elements of the ClauseAssume clause in the case of the operator is instance of NarrowingConversion
-    public static HashSet<String> getContainerNarrowingConversion (Primitive operator, HashSet<String> set) {
-        //special case for arrays
-        if (((NarrowingConversion) operator).getArg() instanceof PrimitiveSymbolicMemberArrayLength || ((NarrowingConversion) operator).getArg() instanceof PrimitiveSymbolicMemberArray) {
-            set.add(operator.toString());
-        }
-        else if (((NarrowingConversion) operator).getArg() instanceof NarrowingConversion) {
-            getContainerNarrowingConversion(((NarrowingConversion) operator).getArg(), set);
-        }
-        else if (((NarrowingConversion) operator).getArg() instanceof WideningConversion) {
-            getContainerWideningConversion(((NarrowingConversion) operator).getArg(), set);
-        }
-        else if (((NarrowingConversion) operator).getArg() instanceof Expression) {
-            getContainer (((NarrowingConversion) operator).getArg(), set);
-        }
-        else {
-            try {
-                set.add(((PrimitiveSymbolicMember) ((NarrowingConversion) operator).getArg()).getContainer().asOriginString());
-            }
-            catch (Exception e) {
-                //give up
-                set.add(splitByDot(((Symbolic) operator).asOriginString()));
-            }
-        }
-        return set;
-    }
-
-    public static String splitByDot (String op) {
-        int index=op.lastIndexOf('.');
+    public static String splitByDot(String originString) {
+        final int index = originString.lastIndexOf('.');
         if (index == -1) {
-            return op;
-        }
-        else {
-            return op.substring(0,index);
+            return originString;
+        } else {
+            return originString.substring(0, index);
         }
     }
 }
