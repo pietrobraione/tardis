695c95ca20167a64b7a6a4c12b1714b036b61b0f
Pietro Braione <pietro.braione@gmail.com>
2023-01-25 15:08:14 +0100

    Small changes

--- master/src/main/java/tardis/implementation/data/EvosuiteResultInputOutputBuffer.java
+++ master/src/main/java/tardis/implementation/data/EvosuiteResultInputOutputBuffer.java
@@ -5,20 +5,15 @@ import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import tardis.framework.QueueInputOutputBuffer;
 import tardis.implementation.evosuite.EvosuiteResult;
 
 public class EvosuiteResultInputOutputBuffer extends QueueInputOutputBuffer<EvosuiteResult> {
-    //private static final Logger LOGGER = LogManager.getFormatterLogger(EvosuiteResultInputOutputBuffer.class);
     private final LinkedBlockingQueue<EvosuiteResult> queueWithPriority = new LinkedBlockingQueue<>();
     
     @Override
     public boolean add(EvosuiteResult item) {
     	if (item.getPathConditionGenerating() == null) {
-    		//LOGGER.info("Adding test case to priority queue: " + item.getTestCase().getClassName());
     		return this.queueWithPriority.add(item);
     	} else {
     		return super.add(item);
@@ -27,7 +22,7 @@ public class EvosuiteResultInputOutputBuffer extends QueueInputOutputBuffer<Evos
     
     @Override
     public List<EvosuiteResult> pollN(int n, long timeoutDuration, TimeUnit timeoutTimeUnit)
-    		throws InterruptedException {
+    throws InterruptedException {
     	final ArrayList<EvosuiteResult> retVal = new ArrayList<>();
     	for (int i = 1; i <= n; ++i) {
     		final EvosuiteResult item = this.queueWithPriority.poll(timeoutDuration, timeoutTimeUnit);
@@ -36,11 +31,6 @@ public class EvosuiteResultInputOutputBuffer extends QueueInputOutputBuffer<Evos
     		}
     		retVal.add(item);
     	}
-    	/*if (retVal.size() > 0) {
-    		for (EvosuiteResult tc: retVal) {
-        		LOGGER.info("Taking test case from priority queue: " + tc.getTestCase().getClassName());    			
-    		}
-    	}*/
     	if (retVal.size() < n) {
     		retVal.addAll(super.pollN(n - retVal.size(), timeoutDuration, timeoutTimeUnit));
     	}
